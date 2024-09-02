56cfa3b56cd94e757671d371843db3dfcef598d5
Robin Ung <Robin_ung@hotmail.com>
2024-09-02 15:00:36 +0000

    Add Profile model, Made Migrations for home

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+
+# Unregister the default User, Group
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+
+# User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Display username
+    fields = ['username']
+
+
+# Register the new User model
+admin.site.register(User, UserAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# User Profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follow = models.ManyToManyField('self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True)
