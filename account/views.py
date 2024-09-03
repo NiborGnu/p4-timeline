d8f849d286596f5bbe31eee880106ecade994000
Robin Ung <Robin_ung@hotmail.com>
2024-09-03 16:02:07 +0000

    Fix views spelling

--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 
-def login(request):
+def login_user(request):
     return render(request, 'account/login_user.html')
 
-def register(request):
+def register_user(request):
     return render(request, 'account/register_user.html')
 
-def logout(request):
+def logout_user(request):
     return render(request, 'account/logout_user.html')
