2738815f9a94fce5b4fefb72327652497b0957b1
Robin Ung <Robin_ung@hotmail.com>
2024-09-05 23:17:08 +0000

    Add follows url

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('profiles/', views.profiles, name='profiles'),
     path('profile/<int:pk>', views.profile, name='profile'),
     path('profile/followers/<int:pk>', views.followers, name='followers'),
+    path('profile/follows/<int:pk>', views.follows, name='follows'),
     path('like/<int:pk>', views.TimePost_like, name='like'),
     path('dislike/<int:pk>', views.TimePost_dislike, name='dislike'),
     path('unfollow/<int:pk>', views.unfollow, name='unfollow'),
