cb505b4da1803c44a0200fe80bfab43310995fb6
Robin Ung <Robin_ung@hotmail.com>
2024-09-05 23:02:50 +0000

    Add followers page

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('profiles/', views.profiles, name='profiles'),
     path('profile/<int:pk>', views.profile, name='profile'),
+    path('profile/followers/<int:pk>', views.followers, name='followers'),
     path('like/<int:pk>', views.TimePost_like, name='like'),
     path('dislike/<int:pk>', views.TimePost_dislike, name='dislike'),
     path('unfollow/<int:pk>', views.unfollow, name='unfollow'),
