d07adf7905dcc186eacabeb841a2999dc82d0d2a
Robin Ung <Robin_ung@hotmail.com>
2024-09-08 19:10:49 +0000

    Add delete and edit urls

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('like/<int:pk>', views.TimePost_like, name='like'),
-    path('dislike/<int:pk>', views.TimePost_dislike, name='dislike'),
+    path('like/<int:pk>', views.timepost_like, name='like'),
+    path('dislike/<int:pk>', views.timepost_dislike, name='dislike'),
     path('unfollow/<int:pk>', views.unfollow, name='unfollow'),
     path('follow/<int:pk>', views.follow, name='follow'),
+    path('delete_timepost/<int:pk>', views.delete_timepost, name='delete_timepost'),
+    path('edit_timepost/<int:pk>', views.edit_timepost, name='edit_timepost'),
 ]
