1baf9c7f2e691a95dff34ac506b8a38ed8079890
Robin Ung <Robin_ung@hotmail.com>
2024-09-10 18:20:46 +0000

    Add Deployment to Heroku

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
