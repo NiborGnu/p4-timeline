48746477e63a3ad13f56f44770445d288fdf6231
Robin Ung <Robin_ung@hotmail.com>
2024-09-10 18:42:58 +0000

    Add herokuapp > allowed_hosts

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', 'p4-timeline-3238b4375b60.herokuapp.com']
 
 
 # Application definition
