db4b43e71b253a514ea2dce8eac0c9eec63bbcce
Robin Ung <Robin_ung@hotmail.com>
2024-09-10 19:45:17 +0000

    enable serving of static files

---
+++ staticfiles/js/base.js
@@ -0,0 +1,23 @@
+// Function to toggle between post view and edit form
+function toggleEdit(id) {
+    var viewElement = document.getElementById("timepost-view-" + id);
+    var editElement = document.getElementById("edit-form-" + id);
+
+    if (viewElement.style.display === "none") {
+        viewElement.style.display = "block"; // Show the TimePost view
+        editElement.style.display = "none"; // Hide the edit form
+    } else {
+        viewElement.style.display = "none"; // Hide the TimePost view
+        editElement.style.display = "block"; // Show the edit form
+    }
+}
+
+// Function to toggle comments section
+function toggleComments(id) {
+    var commentsSection = document.getElementById('comments-section-' + id);
+    if (commentsSection.style.display === 'none' || commentsSection.style.display === '') {
+        commentsSection.style.display = 'block'; // Show comments section
+    } else {
+        commentsSection.style.display = 'none'; // Hide comments section
+    }
+}
