e0908e683e7719bf4cf6d261acee310f800beb59
Robin Ung <Robin_ung@hotmail.com>
2024-09-16 15:25:11 +0000

    Fix problem edit timepost empty text box submition

--- home/forms.py
+++ home/forms.py
@@ -7,8 +7,10 @@ class TimePostForm(forms.ModelForm):
         required=True,
         widget=forms.Textarea(
             attrs={
-                'placeholder': 'Whats happening today?',
+                'placeholder': "What's happening today?",
                 'class': 'form-control',
+                'rows': 4,  # Set default height of textarea
+                'aria-label': 'TimePost body',
             }
         ),
         label="",
@@ -20,6 +22,19 @@ class TimePostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Add a comment...',
+                'class': 'form-control',
+                'rows': 3,  # Set default height of textarea
+                'aria-label': 'Comment body',
+            }
+        ),
+        label="",
+    )
+
     class Meta:
         model = Comment
         fields = ['body']
